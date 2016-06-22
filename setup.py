c790defe9bdae8aacfeebf722de979d7da7477c3
root <dongguosheng179@gmail.com>
2016-06-22 16:21:07 +0800

    update tag num

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'lsh',
     packages = ['lsh'], # the same as the name above
-    version = '0.1',
+    version = '0.1.0',
     description = 'locality-sensitive hashing index, learning to hash',
     author = 'Dong Guosheng',
     author_email = 'dongguosheng179@gmail.com',
     url = 'https://github.com/dongguosheng/lsh', # URL to the github repo
-    download_url = 'https://github.com/dongguosheng/lsh/tarball/0.1',
+    download_url = 'https://github.com/dongguosheng/lsh/tarball/0.1.0',
     install_requires=['numpy', 'bitarray'],
     keywords = ['lsh', 'locality-sensitive hash', 'learning to hash'],
     classifiers = [],
