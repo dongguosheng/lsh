eda4669694cd7868ebcfa423fa7bf4ed9770f878
root <dongguosheng179@gmail.com>
2016-06-22 16:07:04 +0800

    arange file for pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name = 'lsh',
+    packages = ['lsh'], # the same as the name above
+    version = '0.1',
+    description = 'locality-sensitive hashing index, learning to hash',
+    author = 'Dong Guosheng',
+    author_email = 'dongguosheng179@gmail.com',
+    url = 'https://github.com/dongguosheng/lsh', # URL to the github repo
+    download_url = 'https://github.com/dongguosheng/lsh/tarball/0.1',
+    install_requires=['numpy', 'bitarray'],
+    keywords = ['lsh', 'locality-sensitive hash', 'learning to hash'],
+    classifiers = [],
+)
